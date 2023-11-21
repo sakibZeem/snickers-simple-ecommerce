05db3f5ed405deecbe7ce47c0ba75b83fb938127
sakibZeem <anamulsakibzim22@gmail.com>
2023-11-22 01:23:52 +0600

    header added

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Header.css'
+import logo from '../../images/Logo.svg'
+const Header = () => {
+    return (
+        <nav>
+            <img src={logo} alt="" />
+            <ul>
+                <a href="/order">Order</a>
+                <a href="/order-review">Order Review</a>
+                <a href="/inventory">Mange Inventory</a>
+                <a href="/login">Login</a>
+            </ul>
+        </nav>
+    );
+};
+
+export default Header;
